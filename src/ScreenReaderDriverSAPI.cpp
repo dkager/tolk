521aaa3b830ab98785de5451b926e6ac39604f31
Davy Kager <mail@davykager.nl>
2014-01-30 12:27:22 +0100

    Add a basic SAPI driver (still unused and untested)

---
+++ src/ScreenReaderDriverSAPI.cpp
@@ -0,0 +1,54 @@
+/**
+ *  Product:        Tolk
+ *  File:           ScreenReaderDriverSAPI.cpp
+ *  Description:    Driver for the Microsoft Speech API (SAPI).
+ *  Copyright:      (c) 2014, Davy Kager <mail@davykager.nl>
+ *  License:        LGPLv3
+ */
+
+#include "ScreenReaderDriverSAPI.h"
+
+ScreenReaderDriverSAPI::ScreenReaderDriverSAPI() :
+  ScreenReaderDriver(L"SAPI", true, false),
+  controller(NULL)
+{
+  Initialize();
+}
+
+ScreenReaderDriverSAPI::~ScreenReaderDriverSAPI() {
+  Finalize();
+}
+
+bool ScreenReaderDriverSAPI::Speak(const wchar_t *str, bool interrupt) {
+  if (IsActive()) {
+    DWORD flags = SPF_ASYNC | SPF_IS_NOT_XML;
+    if (interrupt) {
+      flags |= SPF_PURGEBEFORESPEAK;
+    }
+    return SUCCEEDED(controller->Speak(str, flags, NULL));
+  }
+  return false;
+}
+
+bool ScreenReaderDriverSAPI::Silence() {
+  if (IsActive()) {
+    const DWORD flags = SPF_ASYNC | SPF_PURGEBEFORESPEAK | SPF_IS_NOT_XML;
+    return SUCCEEDED(controller->Speak(NULL, flags, NULL));
+  }
+  return false;
+}
+
+void ScreenReaderDriverSAPI::Initialize() {
+  if (FAILED(CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_INPROC_SERVER, IID_ISpVoice, (void **)&controller))) {
+    // This is here for symmetry with other drivers
+    // and so compiling /analyze won't throw a warning.
+    return;
+  }
+}
+
+void ScreenReaderDriverSAPI::Finalize() {
+  if (controller) {
+    controller->Release();
+    controller = NULL;
+  }
+}
---
+++ src/ScreenReaderDriverSAPI.h
@@ -0,0 +1,30 @@
+/**
+ *  Product:        Tolk
+ *  File:           ScreenReaderDriverSAPI.h
+ *  Description:    Driver for the Microsoft Speech API (SAPI).
+ *  Copyright:      (c) 2014, Davy Kager <mail@davykager.nl>
+ *  License:        LGPLv3
+ */
+
+#ifndef _SCREEN_READER_DRIVER_SAPI_H_
+#define _SCREEN_READER_DRIVER_SAPI_H_
+
+#include "ScreenReaderDriver.h"
+#include <sapi.h>
+
+class ScreenReaderDriverSAPI : public ScreenReaderDriver {
+public:
+  ScreenReaderDriverSAPI();
+  ~ScreenReaderDriverSAPI();
+  bool Speak(const wchar_t *str, bool interrupt);
+  bool Braille(const wchar_t *str) { return false; }
+  bool Silence();
+  bool IsActive() { return (controller != NULL); }
+  bool Output(const wchar_t *str, bool interrupt) { return Speak(str, interrupt); }
+private:
+  void Initialize();
+  void Finalize();
+  ISpVoice *controller;
+};
+
+#endif // _SCREEN_READER_DRIVER_SAPI_H_
