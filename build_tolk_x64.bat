c1b00f6e60fcaa0e52045ca9718782085f612bca
Davy Kager <mail@davykager.nl>
2014-10-17 14:17:15 +0200

    Remove obscure switches from build script

--- build_tolk_x64.bat
+++ build_tolk_x64.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cl /nologo /c /O2 /EHsc /GL /GR- /MD /vmb /vms /MP /W4 /WL /D_DLL /D_EXPORTING /D_UNICODE /D_USRDLL /D_WINDLL /D_WINDOWS /D_WITH_JNI /DNDEBUG /DWIN64 /DUNICODE /Fobin\x64\ src\*.c src\*.cpp src\java\*.cpp src\reaper\*.cpp
+cl /nologo /c /O2 /EHsc /GL /GR- /MD /MP /W4 /WL /D_DLL /D_EXPORTING /D_UNICODE /D_USRDLL /D_WINDLL /D_WINDOWS /D_WITH_JNI /DNDEBUG /DWIN64 /DUNICODE /Fobin\x64\ src\*.c src\*.cpp src\java\*.cpp src\reaper\*.cpp
 
 rc /nologo /n /d _WIN64 /fo bin\x64\Tolk.res src\Tolk.rc
 
