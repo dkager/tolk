9558a7ba2bab3937f3d223485fe3987401740b09
Davy Kager <mail@davykager.nl>
2015-05-22 12:29:26 +0200

    Use MarshalAs attribute for parameters in the .NET wrapper.

--- src/dotnet/Tolk.cs
+++ src/dotnet/Tolk.cs
@@ -20,9 +20,12 @@ namespace DavyKager {
     [DllImport("Tolk.dll")] private static extern IntPtr Tolk_DetectScreenReader();
     [DllImport("Tolk.dll")] private static extern bool Tolk_HasSpeech();
     [DllImport("Tolk.dll")] private static extern bool Tolk_HasBraille();
-    [DllImport("Tolk.dll")] private static extern bool Tolk_Output(IntPtr ptr, bool interrupt);
-    [DllImport("Tolk.dll")] private static extern bool Tolk_Speak(IntPtr ptr, bool interrupt);
-    [DllImport("Tolk.dll")] private static extern bool Tolk_Braille(IntPtr ptr);
+    [DllImport("Tolk.dll")] private static extern bool Tolk_Output(
+      [MarshalAs(UnmanagedType.LPWStr)]string str, bool interrupt);
+    [DllImport("Tolk.dll")] private static extern bool Tolk_Speak(
+      [MarshalAs(UnmanagedType.LPWStr)]string str, bool interrupt);
+    [DllImport("Tolk.dll")] private static extern bool Tolk_Braille(
+      [MarshalAs(UnmanagedType.LPWStr)]string str);
     [DllImport("Tolk.dll")] private static extern bool Tolk_IsSpeaking();
     [DllImport("Tolk.dll")] private static extern bool Tolk_Silence();
 
@@ -34,52 +37,14 @@ namespace DavyKager {
     public static void Unload() { Tolk_Unload(); }
     public static void TrySAPI(bool trySAPI) { Tolk_TrySAPI(trySAPI); }
     public static void PreferSAPI(bool preferSAPI) { Tolk_PreferSAPI(preferSAPI); }
+    // Prevent the marshaller from freeing the unmanaged string
+    public static string DetectScreenReader() { return Marshal.PtrToStringUni(Tolk_DetectScreenReader()); }
     public static bool HasSpeech() { return Tolk_HasSpeech(); }
     public static bool HasBraille() { return Tolk_HasBraille(); }
+    public static bool Output(string str, bool interrupt = false) { return Tolk_Output(str, interrupt); }
+    public static bool Speak(string str, bool interrupt = false) { return Tolk_Speak(str, interrupt); }
+    public static bool Braille(string str) { return Tolk_Braille(str); }
     public static bool IsSpeaking() { return Tolk_IsSpeaking(); }
     public static bool Silence() { return Tolk_Silence(); }
-
-    public static string DetectScreenReader() {
-      return Marshal.PtrToStringUni(Tolk_DetectScreenReader());
-    }
-
-    public static bool Output(string str, bool interrupt = false) {
-      bool result = false;
-      IntPtr ptr = IntPtr.Zero;
-      try {
-        ptr = Marshal.StringToHGlobalUni(str);
-        result = Tolk_Output(ptr, interrupt);
-      }
-      finally {
-        Marshal.FreeHGlobal(ptr);
-      }
-      return result;
-    }
-
-    public static bool Speak(string str, bool interrupt = false) {
-      bool result = false;
-      IntPtr ptr = IntPtr.Zero;
-      try {
-        ptr = Marshal.StringToHGlobalUni(str);
-        result = Tolk_Speak(ptr, interrupt);
-      }
-      finally {
-        Marshal.FreeHGlobal(ptr);
-      }
-      return result;
-    }
-
-    public static bool Braille(string str) {
-      bool result = false;
-      IntPtr ptr = IntPtr.Zero;
-      try {
-        ptr = Marshal.StringToHGlobalUni(str);
-        result = Tolk_Braille(ptr);
-      }
-      finally {
-        Marshal.FreeHGlobal(ptr);
-      }
-      return result;
-    }
   }
 }
