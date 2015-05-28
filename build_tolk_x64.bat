de24096ff3a663075c990d90c71caee6bc2c5bab
Davy Kager <mail@davykager.nl>
2015-05-28 21:50:28 +0200

    Clean up aand fix the batch files to build the DLLs.

--- build_tolk_x64.bat
+++ build_tolk_x64.bat
@@ -1,9 +1,9 @@
 @echo off
 
-cl /nologo /c /O2 /EHsc /GL /GR- /GS- /Gw /MT /MP /W4 /WL /D_EXPORTING /D_MT /D_UNICODE /D_USRDLL /D_WINDLL /D_WINDOWS /D_WITH_JNI /DNDEBUG /DWIN64 /DUNICODE /Fobin\x64\ src\*.c src\*.cpp src\java\*.cpp src\reaper\*.cpp
+cl /nologo /c /O2 /EHsc /GL /GR- /GS- /Gw /LD /MP /W4 /WL /D_EXPORTING /D_UNICODE /D_WINDOWS /D_WITH_JNI /DNDEBUG /DWIN64 /DUNICODE /Fobin\x64\ src\*.c src\*.cpp src\java\*.cpp src\reaper\*.cpp
 
 rc /nologo /n /d _WIN64 /fo bin\x64\Tolk.res src\Tolk.rc
 
 link /nologo /DLL /INCREMENTAL:NO /LTCG /MACHINE:X64 /MANIFEST:NO /OPT:REF /OPT:ICF /VERSION:"1.0" /OUT:bin\x64\Tolk.dll User32.Lib Ole32.Lib OleAut32.Lib bin\x64\*.obj bin\x64\Tolk.res
 
-del bin\x64\*.obj bin\x64\Tolk.exp bin\x64\Tolk.res
+del bin\x64\*.obj bin\x64\Tolk.res
--- build_tolk_x86.bat
+++ build_tolk_x86.bat
@@ -1,9 +1,9 @@
 @echo off
 
-cl /nologo /c /O2 /EHsc /GL /GR- /GS- /Gw /MT /MP /W4 /WL /D_EXPORTING /D_MT /D_UNICODE /D_USRDLL /D_WINDLL /D_WINDOWS /D_WITH_JNI /DNDEBUG /DWIN32 /DUNICODE /Fobin\x86\ src\*.c src\*.cpp src\java\*.cpp src\reaper\*.cpp
+cl /nologo /c /O2 /EHsc /GL /GR- /GS- /Gw /LD /MP /W4 /WL /D_EXPORTING /D_UNICODE /D_WINDOWS /D_WITH_JNI /DNDEBUG /DWIN32 /DUNICODE /Fobin\x86\ src\*.c src\*.cpp src\java\*.cpp src\reaper\*.cpp
 
 rc /nologo /n /d _WIN32 /fo bin\x86\Tolk.res src\Tolk.rc
 
 link /nologo /DLL /INCREMENTAL:NO /LTCG /MACHINE:X86 /MANIFEST:NO /OPT:REF /OPT:ICF /VERSION:"1.0" /OUT:bin\x86\Tolk.dll User32.Lib Ole32.Lib OleAut32.Lib bin\x86\*.obj bin\x86\Tolk.res
 
-del bin\x86\*.obj bin\x86\Tolk.exp bin\x86\Tolk.res
+del bin\x86\*.obj bin\x86\Tolk.res
